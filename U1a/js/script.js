e3bc2ee97801e297e237aa07158e8361787534c9
HibbaHindawi <hibba04@hotmail.se>
2023-11-06 19:25:25 +0100

    yes

---
+++ U1a/js/script.js
@@ -0,0 +1,18 @@
+/* Uppgift U1 */
+
+// Globala variabler
+let otionsDialog;	    // Element för inställningsdialog
+let rollElem;           // Element för utskrift av antal omkast som återstår
+let resElem;	        // Element för resultat
+let stopBtn;            // Knapp för att stanna
+let playerName = "Du";  // Spelarens namn
+let maxNrOfRolls = 3;   // Valt max antal omkast av tärningar
+let nrOfRolls = 0;      // Antal omkast som återstår
+let sum = 0;            // Summan av kastade tärningar
+// --------------------------------------------------
+// Ta fram referenser till element i gränssnittet och lägg till händelselyssnare.
+function init() {
+	
+} // Slut init
+window.addEventListener("load", init);
+// --------------------------------------------------
